ac5a4b91ce4a291ba549ac88ad7864430f1fd3db
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2022-06-15 21:47:22 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
+use App\Http\Controllers\AdminController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('admin.index');
+    })->name('dashboard');
+});
+
+// Logout Route
+Route::get('/admin/logout', [AdminController::class, 'Logout'])->name('admin.logout');

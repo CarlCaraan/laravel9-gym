6b7baad852838fe001926cd5ae9edcc0dab93d4d
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2022-06-24 22:58:01 +0800

    6/24/22

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             AdminSiteInfoSeeder::class,
             UserSiteInfoTableSeeder::class,
+            AboutTableSeeder::class,
         ]);
     }
 }
